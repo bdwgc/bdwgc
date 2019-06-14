710d0d3ffdd430c67b677ba56f2d68eab54d64f3
Ivan R <iarspider@gmail.com>
2019-06-14 11:24:47 +0200

    Fix 'ISO C++17 does not allow dynamic exception spec' clang-8 error (a cherry-pick of commit 93eecfbf4 from 'release-7_6')
    Issue #287 (bdwgc).
    
    Before this patch, clang 8 (and -std=c++1z) fails for gc_cpp.cc.
    The error message produced is: ISO C++17 does not allow dynamic
    exception specifications.
    
    The "dynamic exception" syntax was declared deprecated in C++11 and
    removed in C++17.
    
    * gc_cpp.cc [GC_NEW_DELETE_NEED_THROW && __clang__
    && __cplusplus >= 201703L] (GC_DECL_NEW_THROW): Define to
    noexcept(false); add comment.

--- gc_cpp.cc
+++ gc_cpp.cc
@@ -37,7 +37,13 @@ built-in "new" and "delete".
 #ifdef GC_NEW_DELETE_NEED_THROW
 # include <new> /* for std::bad_alloc */
 # if (__cplusplus < 201103L || defined(__clang__))
-#   define GC_DECL_NEW_THROW throw(std::bad_alloc)
+#   if __cplusplus < 201703L
+#     define GC_DECL_NEW_THROW throw(std::bad_alloc)
+#   else
+      // The "dynamic exception" syntax was deprecated in C++11
+      // and removed in C++17.
+#     define GC_DECL_NEW_THROW noexcept(false)
+#   endif
 # else
 #   define GC_DECL_NEW_THROW /* empty */
 # endif
