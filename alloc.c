c8eb5c469e90d13ceaadc24478f4aa4f306af3d4
Ivan Maidanski <ivmai@mail.ru>
2025-05-19 20:08:32 +0300

    Fix a typo in HBLKSIZE name in comment (a cherry-pick of commit e13e76c33 from 'master')
    * alloc.c: Fix typo in comment ("HBLKSIZE").

--- alloc.c
+++ alloc.c
@@ -56,7 +56,7 @@
 /*
  * The allocator uses GC_allochblk to allocate large chunks of objects.
  * These chunks all start on addresses which are multiples of
- * HBLKSZ.   Each allocated chunk has an associated header,
+ * HBLKSIZE.   Each allocated chunk has an associated header,
  * which can be located quickly based on the address of the chunk.
  * (See headers.c for details.)
  * This makes it possible to check quickly whether an
