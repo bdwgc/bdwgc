98d7efa1d43f17033ee2a75b555fc1d3a4a86618
Ivan Maidanski <ivmai@mail.ru>
2025-10-10 07:48:32 +0300

    Fix a typo (words duplication) in comment of backgraph.c (fix of commit 3313dcc53)
    * backgraph.c: Fix typo ("simply add them to") in comment.

--- backgraph.c
+++ backgraph.c
@@ -19,10 +19,9 @@
  * data structures; the code is not used during normal garbage collection.
  *
  * One restriction is that we drop all back-edges from nodes with very
- * high in-degree, and simply add them add them to a list of such
- * nodes.  They are then treated as permanent roots.  If this by itself
- * does not introduce a space leak, then such nodes cannot contribute to
- * a growing space leak.
+ * high in-degree, and simply add them to a list of such nodes.  They are
+ * then treated as permanent roots.  If this by itself does not introduce
+ * a space leak, then such nodes cannot contribute to a growing space leak.
  */
 
 #ifdef MAKE_BACK_GRAPH
