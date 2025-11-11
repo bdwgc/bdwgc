80fbf719722ae6c71538145683b5c544f4a047b0
Ivan Maidanski <ivmai@mail.ru>
2025-11-11 18:04:17 +0300

    Remove unneeded exec() from if_not_there utility
    * tools/if_not_there.c: Update title comment; do not include `unistd.h`
    file.
    * tools/if_not_there.c (EXECV_ARGV_T): Do not define.
    * tools/if_not_there.c (main): Print usage info (and exit with
    a non-zero code) if two arguments are given to this utility; do not
    print "Starting command" message; do not call `fflush()`, `execvp()`
    and `exit(1)` (because `argc` is equal to 2); avoid code duplication
    near `fopen()` call.

--- tools/if_not_there.c
+++ tools/if_not_there.c
@@ -1,24 +1,13 @@
 /*
- * A build-time utility used by `Makefile.direct` file.  Conditionally
- * execute the command `argv[2]` if the file `argv[1]` does not exist.
- * If the command is omitted (and the file does not exist), then just
- * exit with a nonzero code.
+ * A build-time utility used by `Makefile.direct` file.  Just exit with
+ * a nonzero code if the file specified by `argv[1]` does not exist.
  */
 
 #define NOT_GCBUILD
 #include "private/gc_priv.h"
 
-#include <unistd.h>
-
 #ifdef __DJGPP__
 #  include <dirent.h>
-#endif
-
-#ifdef __cplusplus
-#  define EXECV_ARGV_T char **
-#else
-/* See the comment in `if_mach.c` file. */
-#  define EXECV_ARGV_T void *
 #endif
 
 int
@@ -30,37 +19,26 @@ main(int argc, char **argv)
 #endif
   const char *fname;
 
-  if (argc < 2 || argc > 3)
+  if (argc != 2)
     goto Usage;
 
   fname = TRUSTED_STRING(argv[1]);
   f = fopen(fname, "rb");
-  if (f != NULL) {
-    fclose(f);
-    return 0;
-  }
-  f = fopen(fname, "r");
-  if (f != NULL) {
+  if (f != NULL || (f = fopen(fname, "r")) != NULL) {
     fclose(f);
     return 0;
   }
 #ifdef __DJGPP__
-  if ((d = opendir(fname)) != 0) {
+  d = opendir(fname);
+  if (d != NULL) {
     closedir(d);
     return 0;
   }
 #endif
-  printf("^^^^Starting command^^^^\n");
-  fflush(stdout);
-  if (argc == 2) {
-    /* The file is missing, but no command is given. */
-    return 2;
-  }
-
-  execvp(TRUSTED_STRING(argv[2]), (EXECV_ARGV_T)(argv + 2));
-  exit(1);
+  /* The file is missing. */
+  return 2;
 
 Usage:
-  fprintf(stderr, "Usage: %s file_name [command]\n", argv[0]);
+  fprintf(stderr, "Usage: %s file_name\n", argv[0]);
   return 1;
 }
