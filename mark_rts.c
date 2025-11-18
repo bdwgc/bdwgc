f59f54258b560ae289e463df15fbf870fe7dad96
Ivan Maidanski <ivmai@mail.ru>
2025-11-18 20:47:51 +0300

    Eliminate 'argument unused' gcc warning in push_all_stack_partially_eager (a cherry-pick of commit a426ffb37 from 'master')
    * mark_rts.c [!THREADS] (GC_push_all_stack_partially_eager): Mark
    `cold_gc_frame` argument as unused (for the case of
    `NEED_FIXUP_POINTER`).

--- mark_rts.c
+++ mark_rts.c
@@ -725,7 +725,7 @@ GC_INNER void GC_push_all_stack_sections(
  * GC_dirty() call.
  */
 STATIC void GC_push_all_stack_partially_eager(ptr_t bottom, ptr_t top,
-                                              ptr_t cold_gc_frame)
+        ptr_t cold_gc_frame GC_ATTR_UNUSED)
 {
 #ifndef NEED_FIXUP_POINTER
   if (GC_all_interior_pointers) {
