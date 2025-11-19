bb24bc9aaf418813ca24b049123aea73bc9c8383
Ivan Maidanski <ivmai@mail.ru>
2025-11-19 08:43:58 +0300

    Eliminate 'GC_push_all_stack not used' gcc warning if fix-up pointers (fix of commit 9ec0f5f6c)
    * include/private/gc_priv.h [NEED_FIXUP_POINTER] (GC_push_all_stack):
    Define as macro (to `GC_push_all_eager`).
    * mark.c (GC_push_all_stack): Do not define if `NEED_FIXUP_POINTER`.

--- include/private/gc_priv.h
+++ include/private/gc_priv.h
@@ -2682,8 +2682,12 @@ GC_INNER GC_bool GC_collection_in_progress(void);
  */
 #define GC_PUSH_ALL_SYM(sym) GC_push_all_eager(&(sym), &(sym) + 1)
 
+#if defined(NEED_FIXUP_POINTER)
+#  define GC_push_all_stack(b, t) GC_push_all_eager(b, t)
+#else
 /* Same as `GC_push_all` but consider interior pointers as valid. */
 GC_INNER void GC_push_all_stack(ptr_t b, ptr_t t);
+#endif
 
 #ifdef NO_VDB_FOR_STATIC_ROOTS
 #  define GC_push_conditional_static(b, t, all) \
