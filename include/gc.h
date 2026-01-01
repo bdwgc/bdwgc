1c809b9b01f91ee95e0bdd214e61b500427a9022
Ivan Maidanski <ivmai@mail.ru>
2026-01-01 22:39:32 +0300

    Fix typo ('contents') in comments (a cherry-pick of commit 7e735494c from 'release-8_0')
    * include/gc.h (GC_realloc, GC_move_disappearing_link): Fix typo
    ("contents") in comment.
    * misc.c [GC_READ_ENV_FILE] (GC_envfile_content, GC_envfile_init):
    Likewise.
    * pthread_stop_world.c (GC_suspend_handler_inner): Likewise.
    * pthread_support.c [LINUX && ARM32] (GC_get_nprocs_present): Likewise.
    * dbg_mlc.c (GC_check_leaked): Likewise.

--- include/gc.h
+++ include/gc.h
@@ -533,8 +533,8 @@ GC_API size_t GC_CALL GC_size(const void * /* obj_addr */) GC_ATTR_NONNULL(1);
 /* The resulting object has the same kind as the original.              */
 /* If the argument is stubborn, the result will have changes enabled.   */
 /* It is an error to have changes enabled for the original object.      */
-/* It does not change the content of the object from its beginning to   */
-/* the minimum of old size and new_size_in_bytes; the content above in  */
+/* It does not change the contents of the object from its beginning to  */
+/* the minimum of old size and new_size_in_bytes; the contents above in */
 /* case of object size growth is initialized to zero (not guaranteed    */
 /* for atomic object type).  The function follows ANSI conventions for  */
 /* NULL old_object (i.e., equivalent to GC_malloc regardless of new     */
@@ -1161,7 +1161,7 @@ GC_API int GC_CALL GC_move_disappearing_link(void ** /* link */,
         /* GC_general_register_disappearing_link (or            */
         /* GC_register_disappearing_link).  Does not change the */
         /* target object of the weak reference.  Does not       */
-        /* change (*new_link) content.  May be called with      */
+        /* change contents of (*new_link).  May be called with  */
         /* new_link equal to link (to check whether link has    */
         /* been registered).  Returns GC_SUCCESS on success,    */
         /* GC_DUPLICATE if there is already another             */
