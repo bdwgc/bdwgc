89bce30720b382b82a2aa7ba7a1c85ed7fe28339
Ivan Maidanski <ivmai@mail.ru>
2026-03-05 09:10:41 +0300

    Fix GC_use_threads_discovery after implicit GC initialization in gctest (a cherry-pick of commit 1849a12fb from 'master')
    * tests/test.c [(GC_WIN32_THREADS && !GC_PTHREADS && GC_DLL
    && !MSWINCE || GC_PTHREADS && DARWIN && !DARWIN_DONT_PARSE_STACK)
    && !GC_NO_THREADS_DISCOVERY && !THREAD_LOCAL_ALLOC] (main): Do not call
    `GC_use_threads_discovery()` if the collector is already initialized.

--- tests/test.c
+++ tests/test.c
@@ -2267,9 +2267,11 @@ DWORD __stdcall thr_window(void * arg GC_ATTR_UNUSED)
 # endif
 # if defined(GC_DLL) && !defined(GC_NO_THREADS_DISCOVERY) \
         && !defined(MSWINCE) && !defined(THREAD_LOCAL_ALLOC)
-    GC_use_threads_discovery();
+    if (!GC_is_init_called()) {
+      GC_use_threads_discovery();
                 /* Test with implicit thread registration if possible. */
-    GC_printf("Using DllMain to track threads\n");
+      GC_printf("Using DllMain to track threads\n");
+    }
 # endif
   GC_COND_INIT();
   enable_incremental_mode();
@@ -2399,8 +2401,10 @@ int main(void)
 #   if defined(GC_DARWIN_THREADS) && !defined(GC_NO_THREADS_DISCOVERY) \
         && !defined(DARWIN_DONT_PARSE_STACK) && !defined(THREAD_LOCAL_ALLOC)
       /* Test with the Darwin implicit thread registration. */
-      GC_use_threads_discovery();
-      GC_printf("Using Darwin task-threads-based world stop and push\n");
+      if (!GC_is_init_called()) {
+        GC_use_threads_discovery();
+        GC_printf("Using Darwin task-threads-based world stop and push\n");
+      }
 #   endif
     GC_set_markers_count(0);
     GC_COND_INIT();
