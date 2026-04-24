a407061abadeba1c0f229eabb3c789dadbd46c77
Ivan Maidanski <ivmai@mail.ru>
2026-04-24 23:13:08 +0300

    Fix crash in backtrace() when called from GC_debug_generic_malloc_inner (fix of commit 963c85ec9)
    Issue #915 (bdwgc).
    
    `ADD_CALL_CHAIN_SAFE()` should be used both by
    `GC_debug_generic_malloc_inner()` and
    `GC_debug_generic_malloc_inner_ignore_off_page()`.
    
    * dbg_mlc.c [DBG_HDRS_ALL] (GC_debug_generic_malloc_inner): Call
    `ADD_CALL_CHAIN_SAFE()` instead of `ADD_CALL_CHAIN()`.

--- dbg_mlc.c
+++ dbg_mlc.c
@@ -590,7 +590,7 @@ GC_API GC_ATTR_MALLOC void * GC_CALL
         GC_start_debugging_inner();
     }
     result = GC_store_debug_info_inner(base, (word)lb, "INTERNAL", 0);
-    ADD_CALL_CHAIN(base, GC_RETURN_ADDR);
+    ADD_CALL_CHAIN_SAFE(base, GC_RETURN_ADDR);
     return result;
   }
 
