addefc07d6e57ab489b7008a9b59fa6739d95701
Ivan Maidanski <ivmai@mail.ru>
2026-06-20 11:53:57 +0300

    Fix user-defined signals drop by marker threads on Cygwin (fix of commit 5569289a4)
    * include/private/gcconfig.h [!NO_MARKER_SPECIAL_SIGMASK
    && GC_PTHREADS_PARAMARK && GC_WIN32_THREADS]
    (NO_MARKER_SPECIAL_SIGMASK): Do not define if `CYGWIN`.

--- include/private/gcconfig.h
+++ include/private/gcconfig.h
@@ -2865,9 +2865,10 @@ EXTERN_C_BEGIN
 #  define GC_PTHREADS_PARAMARK
 #endif
 
-#if !defined(NO_MARKER_SPECIAL_SIGMASK)                                 \
-    && (defined(NACL) || defined(GC_WIN32_PTHREADS)                     \
-        || (defined(GC_PTHREADS_PARAMARK) && defined(GC_WIN32_THREADS)) \
+#if !defined(NO_MARKER_SPECIAL_SIGMASK)                                \
+    && (defined(NACL) || defined(GC_WIN32_PTHREADS)                    \
+        || (defined(GC_PTHREADS_PARAMARK) && defined(GC_WIN32_THREADS) \
+            && !defined(CYGWIN))                                       \
         || defined(GC_NO_PTHREAD_SIGMASK))
 /*
  * Either there is no `pthread_sigmask()`, or the GC marker thread cannot
