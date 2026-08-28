bb49e9e26dcdb41d20f11117be8a93c5a19f30c2
Ivan Maidanski <ivmai@mail.ru>
2026-08-29 00:18:57 +0300

    Fix various typos in comments of cord headers (a cherry-pick of commit 5019e1f7f from 'release-8_0')
    * include/cord.h (CORD_cat): Fix file name (`string.h`) in comment.
    * include/cord.h (CORD_dump): Fix typo ("implementation-defined") in
    comment.
    * include/cord.h (CORD_from_file): Change "descriptor" to "file" in
    comment; change "file pointer" to "file position" in comment.
    * include/cord.h (CORD_from_file_eager): Fix typo
    ("file will be closed") in comment.
    * include/cord_pos.h (CORD_POS_INVALID): Fix typo ("the position")
    in comment.
    * include/ec.h (CORD_ec_append_cord): Fix typo ("the structure") in
    comment.

--- include/ec.h
+++ include/ec.h
@@ -60,8 +60,8 @@ void CORD_ec_flush_buf(CORD_ec x);
                         ? (CORD_ec_flush_buf(x), 0) : 0), \
                  (void)(*(x)[0].ec_bufptr++ = (c)))
 
-/* Append a cord to an extensible cord.  Structure remains shared with  */
-/* original.                                                            */
+/* Append a cord to an extensible cord.  The structure remains shared   */
+/* with the original.                                                   */
 void CORD_ec_append_cord(CORD_ec x, CORD s);
 
 # endif /* EC_H */
